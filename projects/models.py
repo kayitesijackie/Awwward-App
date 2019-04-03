84c008f3a71cb28f8ef4d4ab9f193392397b51eb
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-03 11:24:51 +0200

    add class Project

--- projects/models.py
+++ projects/models.py
@@ -26,3 +26,22 @@ class UserProfile(models.Model):
         return self.user.username
 
 
+class Project(models.Model):
+    project_title = models.CharField(max_length=40)
+    project_description = HTMLField()
+    landing_page = models.ImageField(upload_to='landing_pages')
+    live_site = URLOrRelativeURLField()
+    user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.project_title
