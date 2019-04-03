3cb066d827824fa9163317c799f3f0fc8edfb5fb
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-03 11:27:31 +0200

    import render, redirect, models, db.models, forms, serializer and APIView

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Max,F
+from .models import UserProfile,Project
+from .forms import NewProjectForm,VoteForm,ProfileEditForm
+from django.urls import reverse
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
+
+
 
 # Create your views here.
