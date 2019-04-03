16b6f7228ad5b322c967a21d7345dee6853883b4
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-03 11:45:12 +0200

    add class ProfileSerializer and its fields

--- projects/serializer.py
+++ projects/serializer.py
@@ -2,3 +2,8 @@ from rest_framework import serializers
 from .models import UserProfile,Project
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('user','bio','profile_pic','projects')
+
