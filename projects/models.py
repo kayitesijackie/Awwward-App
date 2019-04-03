1771391e722d189ab6dcbcacabca151be5ba0929
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-03 11:24:25 +0200

    add class UserProfile

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,24 @@ from datetime import datetime
 from tinymce.models import HTMLField
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_photos',null=True,blank=True)
+    bio = HTMLField()
+    contact=models.CharField(max_length=12)
+    projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()
+
+    def __str__(self):
+        return self.user.username
+
+
