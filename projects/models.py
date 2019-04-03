5430f56297b3d5daebb1844255d841dfb0e68c03
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-03 11:23:37 +0200

    import User, datetime and HTMLFiels

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from url_or_relative_url_field.fields import URLOrRelativeURLField
+from datetime import datetime
+from tinymce.models import HTMLField
 
 # Create your models here.
