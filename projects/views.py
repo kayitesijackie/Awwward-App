49d5e8c0d8ce9490a17072efc4b0306ef94a6873
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-03 11:36:26 +0200

    add two classes of APIView

--- projects/views.py
+++ projects/views.py
@@ -94,3 +94,14 @@ def project(request,project_id):
         form = VoteForm()
     return render(request,'project.html',{'form':form,'project':project,'rating':rating})
 
+class ProfileList(APIView):
+    def get(self,request,format=None):
+        all_users = UserProfile.objects.all()
+        serializers = ProfileSerializer(all_users,many=True)
+        return Response(serializers.data)
+
+class ProjectList(APIView):
+    def get(self,request,format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects,many=True)
+        return Response(serializers.data)
