740478e0f4b144e829d173f491faa8dd264fae5c
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-04-04 12:13:36 +0200

    change the fields length of bio and live_site columns

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from url_or_relative_url_field.fields import URLOrRelativeURLField
 from datetime import datetime
 from tinymce.models import HTMLField
 
@@ -8,7 +7,7 @@ from tinymce.models import HTMLField
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='profile_photos',null=True,blank=True)
-    bio = HTMLField()
+    bio = models.CharField(max_length=200)
     contact=models.CharField(max_length=12)
     projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
 
@@ -30,7 +29,7 @@ class Project(models.Model):
     project_title = models.CharField(max_length=40)
     project_description = HTMLField()
     landing_page = models.ImageField(upload_to='landing_pages')
-    live_site = URLOrRelativeURLField()
+    live_site = models.URLField(max_length=250)
     user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
     design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
